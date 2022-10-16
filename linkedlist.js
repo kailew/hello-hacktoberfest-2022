7c4710c5c7262250262316e8d0f83ec42e43d180
Kai Lew <79333361+kailew@users.noreply.github.com>
2022-10-15 19:55:23 -0700

    Reverse a Singly Linkedlist

---
+++ linkedlist.js
@@ -0,0 +1,33 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+
+
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+   
+    reverse(){
+      var node = this.head;
+      this.head = this.tail;
+      this.tail = node;
+      var next;
+      var prev = null;
+      for(var i = 0; i < this.length; i++){
+        next = node.next;
+        node.next = prev;
+        prev = node;
+        node = next;
+      }
+      return this;
+    }
+    
+    
+}
